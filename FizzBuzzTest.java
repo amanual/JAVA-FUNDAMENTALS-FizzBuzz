4ae1667ba31693d1b7f67a0a57f7e091d4bcdda5
Amanual <amanual2@gmail.com>
2017-10-30 14:55:40 -0400

    added Java/Fundam.../FizzBuzz

---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz result = new FizzBuzz();
+        String output = result.fizzBuzz(15);
+        System.out.println(output);
+        
+    }
+}
